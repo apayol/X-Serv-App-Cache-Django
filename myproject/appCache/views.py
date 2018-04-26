30be3437b921fc661ba31deb703b391a88e3b707
apayol <apayolmontero@gmail.com>
2018-04-26 20:13:35 +0200

    Terminada

--- myproject/appCache/views.py
+++ myproject/appCache/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from appCache.models import Page
+from urllib.request import urlopen, URLError
 
 # Create your views here.
 
+def http(url):
+    if (url.startswith("http://")) or (url.startswith("https://")):
+        return url
+    else:
+        url = ("http://") + url
+    return url
+
 def process (request, resource):
-    respuesta = "Process"
-    #proceso url, veo si está en dicc, introduzco al dicc (if), y muestro content
+    try: # está guardada
+        pagina = Page.objects.get(url=resource)
+        respuesta = pagina.content
+    except Page.DoesNotExist:
+        respuesta = "Url no guardada en diccionario"
+        url = http(resource)
+        try: 
+            with urlopen(url) as nf:
+                cont = nf.read()
+                pagina = Page(url = resource, content = cont)
+                respuesta += "<br/>Guardando..."
+                pagina.save() #introduzco a mi dicc.
+                respuesta += " guardada correctamente.<br/>Refresque para ir al sitio."
+        except URLError:
+            respuesta = "Url incorrecta"
     return HttpResponse (respuesta)
 
 def error (request,resource):
